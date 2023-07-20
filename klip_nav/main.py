df375629145beb1f2bdd09472f7c17c6cf581bcc
skogsgren <6107181+skogsgren@users.noreply.github.com>
2023-07-21 00:30:47 +0200

    remove redundant variable declaration

--- klip_nav/main.py
+++ klip_nav/main.py
@@ -2,13 +2,14 @@ from klip_nav.tui import App
 from klip_nav.utils import SortedClippings
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(
         prog="klip-nav", description="TUI for kindle clippings"
     )
     parser.add_argument("clippings_file")
     args = parser.parse_args()
-    runit = App(
+    App(
         clippings=SortedClippings(args.clippings_file),
         filename=args.clippings_file,
     )
